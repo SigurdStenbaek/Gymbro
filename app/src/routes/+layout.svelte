88f70935feb5fa9cd270f8cb0eea8474082e285d
Thor Stenbæk <Thor.Stenbak@dss.dep.no>
2026-05-30 22:09:54 +0200

    Fixed title

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,17 +2,18 @@
   import "../app.css";    
   import { Heading, Footer } from "flowbite-svelte"; 
 
+  const Name = "Gymrat®";
   let { children } = $props();
 </script>
     
 <svelte:head>
-  <title>Gymbro</title>
+  <title>Gymrat</title>
 </svelte:head>
 
 <main>
-  <Heading tag="h1" class="bg-white p-4" style="height:75px">GymRat®</Heading>
+  <Heading tag="h1" class="bg-white p-4" style="height:75px">{Name}</Heading>
     {@render children()}
   <Footer class="p-4" style="height:60px">
-    2026 GymRat® All Rights Reserved
+    2026 {Name} All Rights Reserved
   </Footer>	
 </main>
